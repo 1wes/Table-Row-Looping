7e5c8123566610b6b087839e6b7654041d722944
1wes <okemwawes@gmail.com>
2022-06-13 17:34:52 +0300

    loop through the array of data

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,11 @@ $(()=>{
             success:(data,status)=>{
 
                 let services_list=JSON.parse(data);
+
+                // loop through the data obtained from the database;
+                for(let i=0; i<=services_list.length;i++){
+                    console.log(services_list[i]);
+                }
                 
             }
         })
