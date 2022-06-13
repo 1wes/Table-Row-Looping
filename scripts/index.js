6e52c8f83fd6e9976dce553547b03849052ea4c2
1wes <okemwawes@gmail.com>
2022-06-13 20:01:17 +0300

    display list of services on button click

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,15 @@ $(()=>{
 
                 // loop through the data obtained from the database;
                 for(let i=0; i<=services_list.length;i++){
-                    console.log(services_list[i]);
+
+                    $("#service-table-body").append(`
+                    <tr>
+                        <td></td> //for the row number counter counter
+                        <td id="service-name">${services_list[i].name}</td>
+                        <td id="service-cost">${services_list[i].cost}</td>
+                        <td><input class="waiver" type="text" placeholder="Enter waiver amount" name="waiver-amount" ></td> //amount waived
+                        <td></td> //blance to pay
+                    </tr>`)
                 }
                 
             }
